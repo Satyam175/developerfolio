3b19fa6cc5a1fad483e0c50907b5cc84bf07af25
Satyam Kaushik <kaushiksatyam54@gmail.com>
2024-07-06 13:12:59 +0530

    updated resume name

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -41,8 +41,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
+                    href={require("./Satyam Kaushik Resume.pdf")}
+                    download="Satyam Kaushik Resume.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
